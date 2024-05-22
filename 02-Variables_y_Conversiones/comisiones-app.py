9908d350ebf45f7be80a94cb6dabdb31447416f1
danielsevilla997 <danielsevilla997@gmail.com>
2024-05-22 12:13:34 +0000

    prueba idx

--- 02-Variables_y_Conversiones/comisiones-app.py
+++ 02-Variables_y_Conversiones/comisiones-app.py
@@ -1,5 +1,5 @@
 #Vendedores ganan de comisiones del 13% de las ventas totales. Pregunta del nombre e total.
 nombre = input("Cual es tu nombre: ")
-ventas = float(input("Cuanto has vendido en total este mes: "))
+ventas = float(input("Cuanto has vendido en total este mes:  "))
 
 print("El total de tu sueldo de comisiones es igual a: {} €".format(round(ventas*13/100, 2)))
